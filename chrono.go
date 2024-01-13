24035643f391d564a463572ede9ee473c7f9ed37
Jonathan Marin <schanejin@gmail.com>
2024-01-13 19:07:53 +0100

    Add Reset() and Stop() methods.

--- chrono.go
+++ chrono.go
@@ -7,7 +7,9 @@ import (
 )
 
 type Chrono struct {
-	start time.Time
+	start   time.Time
+	stop    time.Time
+	elapsed time.Duration
 }
 
 func New() *Chrono {
@@ -16,9 +18,25 @@ func New() *Chrono {
 	}
 }
 func (c *Chrono) ElapsedTime() time.Duration {
-	return time.Since(c.start)
+	c.elapsed = time.Since(c.start)
+	return c.elapsed
 }
-func (c *Chrono) Stop()  {}
+
+// Stop stops the timer. To resume the timer Start() needs to be called again
+func (c *Chrono) Stop() {
+	c.elapsed = c.ElapsedTime()
+	c.stop = time.Now()
+}
+
+// Start resumes or starts the timer. if a Stop() was invoked it resumes the
+// timer. if a Reset() was invoked it starts a nes session.
+func (c *Chrono) Start() {
+	if c.start.IsZero() { //reseted
+		c.start = time.Now()
+	} else { //stopped
+		c.start = c.start.Add(time.Since(c.stop))
+	}
+}
 func (c *Chrono) Pause() {}
 func (c *Chrono) Reset() {
 	c.start = time.Time{}
